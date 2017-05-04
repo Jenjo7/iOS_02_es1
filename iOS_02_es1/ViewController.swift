31bf8adbcf248c49a2c052212aef6d5c8f12e800
Gianni Savini <gianni.savini@studio.unibo.it>
2017-05-04 15:58:27 +0200

    I've Created a very simple view with Native element of Xcode IDE (switcher, slider, textField, Stepper), with simple style changes ;)

--- iOS_02_es1/ViewController.swift
+++ iOS_02_es1/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txvName: UITextField!
+    @IBOutlet weak var lblSons: UILabel!
+    @IBOutlet weak var SonsStepper: UIStepper!
+    @IBOutlet weak var lblMarried: UILabel!
+    @IBOutlet weak var lblAge: UILabel!
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var sldAge: UISlider!
+    @IBOutlet weak var swtMarried: UISwitch!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func setSonsNumbers(_ sender: UIStepper) {
+        lblSons.text = Int(sender.value).description
+    }
+    
 
+    @IBAction func setAge(_ sender: UISlider) {
+        lblAge.text = Int(sldAge.value).description
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txvName.resignFirstResponder()
+        return true
+    }
+    @IBAction func setMarried(_ sender: UISwitch) {
+        lblMarried.text = swtMarried.isOn ? "Sì" : "No"
+    }
+    
+    
 
 }
 
